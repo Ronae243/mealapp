0ed0f01c7aee0fdb95c7fad825ec1c48827b55f0
Ronae243 <ronae2004@gmail.com>
2023-08-09 15:32:43 -0500

    added drawer and filters

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,9 +3,12 @@ import 'package:menu_app/data/dummy_data.dart';
 import 'package:menu_app/models/category.dart';
 import 'package:menu_app/screens/meals.dart';
 import 'package:menu_app/widgets/category_grid_item.dart';
+import '../models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -17,6 +20,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
@@ -24,11 +28,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
-      body: GridView(
+    return  GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -46,7 +46,6 @@ class CategoriesScreen extends StatelessWidget {
               },
             )
         ],
-      ),
-    );
+      );
   }
 }
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:menu_app/screens/categories.dart';
+import 'package:menu_app/screens/fliter.dart';
+import 'package:menu_app/screens/meals.dart';
+import 'package:menu_app/models/meal.dart';
+import 'package:menu_app/widgets/main_drawer.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+  @override
+  State<TabsScreen> createState() {
+    return _TabScreenState();
+  }
+}
+
+class _TabScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  void _toggleMealFavouriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favourite');
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('Marked as a favourite!');
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _setScreen(String identifier) {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FilterScreen(),
+        ),
+      );
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavouriteStatus,
+    );
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+          meals: _favoriteMeals, onToggleFavorite: _toggleMealFavouriteStatus);
+      activePageTitle = 'Your Favourites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
